376f4609912acac26c3d9dfa33754afea32b5185
loehnertz <jakob2102@gmail.com>
2018-01-04 00:24:53 +0100

    Added the basic setup for future routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const Express = require('express');
 const BodyParser = require('body-parser');
 
 const app = Express();
+const router = require('./routes/router');
 
 const PORT = 3000;
 const FRONTEND_PATH = 'frontend';
@@ -9,6 +10,8 @@ const FRONTEND_PATH = 'frontend';
 
 app.use(BodyParser.urlencoded({extended: true}));
 app.use(BodyParser.json());
+
+app.use('/api', router);
 
 app.listen(3000, () => {
     console.log(`Server listening on port ${PORT}!`);
