b98ca2e897d87cb1ac73bfdb88edb37bec803bd8
loehnertz <jakob2102@gmail.com>
2018-01-04 00:24:18 +0100

    Integrated the 'body-parser'

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const Express = require('express');
+const BodyParser = require('body-parser');
 
 const app = Express();
 
 const PORT = 3000;
+const FRONTEND_PATH = 'frontend';
 
 
-app.use(Express.static('frontend'));
+app.use(BodyParser.urlencoded({extended: true}));
+app.use(BodyParser.json());
 
 app.listen(3000, () => {
     console.log(`Server listening on port ${PORT}!`);
