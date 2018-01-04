3ba20a437c8a9d911142b2846e0b04ba9ddc4062
loehnertz <jakob2102@gmail.com>
2018-01-04 01:41:18 +0100

    Added the 'MongoDB' to the 'server.js'

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const Express = require('express');
 const BodyParser = require('body-parser');
+const Mongoose = require('mongoose');
 
 const app = Express();
 const router = require('./routes/router');
 
 const PORT = 3000;
 const FRONTEND_PATH = 'frontend';
+const MONGO_HOST_NAME = 'localhost';
+const MONGO_DATABASE_NAME = 'zaster';
 
+
+Mongoose.Promise = global.Promise;
+Mongoose.connect(`mongodb://${MONGO_HOST_NAME}/${MONGO_DATABASE_NAME}`);
 
 app.use(BodyParser.urlencoded({extended: true}));
 app.use(BodyParser.json());
