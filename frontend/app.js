191ae1f8f849e9e0acd3ea9778fcb5af4adfc652
loehnertz <jakob2102@gmail.com>
2018-01-04 02:31:42 +0100

    Added constants for the API endpoints

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,17 @@
-Vue.config.debug = true;
-Vue.config.devtools = true;
+// Constants
+const apiBaseEndpoint = 'http://localhost:3000/api';
+const apiEndpoints = {
+    finances: '/finances',
+    reports: '/reports',
+};
 
-
+// Vue instance
 let app = new Vue({
     el: '#app',
     data: {
+
+    },
+    methods: {
 
     }
 });
