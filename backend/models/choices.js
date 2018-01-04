1c3cf3f71791c0a5935731441afccec2ca46753b
loehnertz <jakob2102@gmail.com>
2018-01-04 15:07:14 +0100

    Added a file that holds the choices for the 'enums' of the 'Finances' model

---
+++ backend/models/choices.js
@@ -0,0 +1,15 @@
+module.exports = {
+    types: [
+        "income",
+        "expense",
+    ],
+    categories: [
+        "food",
+        "electronics",
+        "drugs",
+        "pets",
+        "rent",
+        "salary",
+        "clothing",
+    ],
+};
